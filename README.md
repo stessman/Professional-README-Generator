be691771d53b9187592d3114b4613cdf5bf815a7
Scott T <scott.tessm@gmail.com>
2021-06-15 17:47:11 -0500

    Completed all requirements for README Generator

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Professional-README-Generator
+# Unit 09 Homework : Professional-README-Generator
+
+This is the homework assignment for Unit 09 NodeJS
+
+Table of Contents
+- [Images](#image-examples)
+- [Description](#description)
+- [Installation](#installation-instructions)
+- [Requirements](#requirements)
+- [Usage](#usage-information)
+
+## Image Examples
+<img src="assets\img\READMEGen1.PNG">
+<img src="assets\img\READMEGen2.PNG">
+
+## Description
+This project was created to assist people that want an easy way to create quality README files for their projects with little thought. After the necessary installations are completed the user is then able to run a command and is then presented with a number of prompts regarding information about their project. After these prompts are answered then a README is automatically created for them based on the user's answers.
+
+## Installation Instructions
+Open the Command Prompt and change your directory to the top level of this application. Once at that directory, run 'npm init -y.' Once this is completed you will need to run 'npm i inquirer.' Once this is done you should be good to go.
+
+## Requirements
+- When the user is prompted for about their application repository, a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions.
+- When the user enters their project title, then this is displayed as the title of the README.
+- When the user enters a description, installation instructions, usage information, contribution guidelines, and test instructions, then this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests. 
+- When the user chooses a license for their application from a list of options, then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under.
+- When the user enters their GitHub username, then this is added to the section of the README entitled Questions, with a link to my GitHub profile.
+- When the user enters their email address, then this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions.
+- When the user clicks on the links in the Table of Contents, then they are taken to the corresponding section of the README.
+
+
+## Usage Information
+After the necessary installations are completed all the user needs to do is navigate to the top level directory of this project in the Command Prompt. Once there, the user should run 'node index.js.' Once entered, the user will be prompted a number of questions regarding information about their project. Once all of the prompts are answered then a README file will be automatically created for them.
+
+Link to the live site: [Weather Dashboard](https://stessman.github.io/06-Weather-Dashboard/)
+Link to a 
---
+++ index.js
@@ -0,0 +1,150 @@
+const fs =  require('fs');
+const inquirer = require('inquirer');
+
+let licenseEmblemPath;
+let licenseInfo;
+
+// Uses inquirer to get information from user about their README
+inquirer.prompt ([
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'githubUsername'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'emailAddress'
+    },
+    {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'projectTitle'
+    },
+    {
+        type: 'input',
+        message: 'Please enter a short description for your project:',
+        name: 'projectDescription'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installInstructions'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repository?',
+        name: 'usageInformation'
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines of your project?',
+        name: 'contributionGuidelines'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'testInstructions'
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have? ',
+        choices:  [
+            'MIT','Apache', 'ISC'
+        ],
+        name: 'license'
+    }
+]).then((data) => {
+    pickLicenseInfo(data.license);
+    const finalREADMEText = READMEText(data, licenseEmblemPath, licenseInfo);
+    fs.appendFile('ExampleREADME.md', finalREADMEText, (err) =>
+    err ? console.error(err) : console.log('File Created/Updated')
+    );
+});
+
+// Takes in user input data and creates the text for the README
+const READMEText = (data, licenseEmblemPath, licenseInfo) => 
+`
+# ${data.projectTitle}
+${licenseEmblemPath}
+
+Table of Contents
+- [Description](#description)
+- [Installation](#installation-instructions)
+- [Usage](#usage-information)
+- [Contributing](#contribution-guidelines)
+- [Tests](#test-instructions)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+${data.projectDescription}
+
+## Installation Instructions
+${data.installInstructions}
+
+## Usage Information
+${data.usageInformation}
+
+## Contribution Guidelines
+${data.contributionGuidelines}
+
+## Test Instructions
+${data.testInstructions}
+
+## License
+${licenseInfo}
+
+## Questions
+If you have any questions feel free to contact me at:  
+GitHub: https://github.com/${data.githubUsername}  
+Email: ${data.emailAddress}
+`
+
+// Takes in the user's license selection and creates variables with needed information for the README
+const  pickLicenseInfo = userPickedLicense => {
+    switch(userPickedLicense){
+        case 'MIT':
+            licenseEmblemPath = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+            licenseInfo = `The MIT License (MIT)
+
+            Copyright (c)
+            
+            Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+            
+            The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+            
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+            
+            Source: http://opensource.org/licenses/MIT`;
+            break;
+        case 'Apache':
+            licenseEmblemPath = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+            licenseInfo = `Copyright 2020 - 2021
+
+            Licensed under the Apache License, Version 2.0 (the "License");
+            you may not use this file except in compliance with the License.
+            You may obtain a copy of the License at
+            
+               http://www.apache.org/licenses/LICENSE-2.0
+            
+            Unless required by applicable law or agreed to in writing, software
+            distributed under the License is distributed on an "AS IS" BASIS,
+            WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+            See the License for the specific language governing permissions and
+            limitations under the License.`;
+            break;
+        case 'ISC':
+            licenseEmblemPath = `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+            licenseInfo = `Copyright (c) 2021
+
+            Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+            
+            THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+            
+            Source: http://opensource.org/licenses/ISC`;
+            break;
+            default:
+                console.log('Enter a valid license.');
+    }
+}
